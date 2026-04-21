b5f23bf1599584e0f0099b8bd63192ac8eea0331
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-21 19:59:19 -0300

    ✨ feat: add signup and account retrieval endpoints with database integration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,43 @@
-export function sum(a: number, b: number) {
-  return a + b;
+import express from "express";
+import pgp from "pg-promise";
+import crypto from "crypto";
+
+const PORT = 3000;
+
+function main() {
+  const app = express();
+  app.use(express.json());
+
+  const connection = pgp()("postgres://postgres:postgres@localhost:6543/app");
+
+
+  app.post("/signup", async (req, res) => {
+    const account = {
+      accountId: crypto.randomUUID(),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      document: req.body.document
+    }
+
+    await connection.query("insert into ccca.account (account_id, name, email, password, document) values ($1, $2, $3, $4, $5)", [account.accountId, account.name, account.email, account.password, account.document]);
+
+    res.status(201).json({ accountId: account.accountId });
+  });
+
+  app.get("/accounts/:accountId", async (req, res) => {
+
+    console.log("Getting account", req.params.accountId);
+    const accounts = await connection.query("select * from ccca.account");
+    console.log("Accounts", accounts);
+
+    const [account] = await connection.query("select * from ccca.account where account_id = $1", [req.params.accountId]);
+
+    console.log("Account", account);
+    res.status(200).json(account);
+  });
+
+  app.listen(PORT, () => console.log(`🚀 server started on port ${PORT}`));
 }
+
+main();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,22 @@
-import { sum } from "../src/main";
+import axios from "axios";
 
-test("Deve somar dois numeros", () => {
-  const result = sum(1, 2);
+test("Deve criar uma conta", async () => {
+  const input = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    document: "12345678900",
+    password: "12345678"
+  }
 
-  expect(result).toBe(3);
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  expect(signupResponse.status).toBe(201);
+  expect(signupResponse.data.accountId).toBeDefined();
+
+  const getAccountResponse = await axios.get(`http://localhost:3000/accounts/${signupResponse.data.accountId}`);
+
+  expect(getAccountResponse.status).toBe(200);
+  expect(getAccountResponse.data.account_id).toBe(signupResponse.data.accountId);
+  expect(getAccountResponse.data.name).toBe(input.name);
+  expect(getAccountResponse.data.email).toBe(input.email);
+  expect(getAccountResponse.data.document).toBe(input.document);
 });
