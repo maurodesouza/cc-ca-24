efcc9f1f6124145e762f8ec7e24ba903e179be88
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-22 14:35:23 -0300

    ⚡ chore: add frontend with vite + react template

---
+++ frontend/react/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>Ahhh ... Hellow!??</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
