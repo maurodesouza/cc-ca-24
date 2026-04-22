48ed1b82a7855b5a36fed4cdb23729275b8ffd1a
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-22 15:10:38 -0300

    🧪 test: add signup e2e test

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const validInput = {
+  name: "John Doe",
+  email: "john.doe@example.com",
+  document: "85486231016",
+  password: "Password123"
+}
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Deve criar uma conta', async ({ page }) => {
+  await page.goto('http://localhost:5173');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill(validInput.name);
+  await page.getByRole('textbox', { name: 'Email' }).fill(validInput.email);
+  await page.getByRole('textbox', { name: 'Document' }).fill(validInput.document);
+  await page.getByRole('textbox', { name: 'Password' }).fill(validInput.password);
+  await page.getByRole('button', { name: 'Create account' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.locator('text=Success')).toBeVisible();
 });
