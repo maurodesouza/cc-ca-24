8701abe831c4cc59d6b2c7a6b82a57b0d7d4c64f
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-21 21:05:29 -0300

    ✨ feat: add name validation function with tests

---
+++ test/is-valid-name.test.ts
@@ -0,0 +1,21 @@
+import { isValidName } from "../src/is-valid-name";
+
+test.each([
+    "Mauro de Souza",
+    "Antonio Carlos",
+    "Eloy Casagrande"
+])("Deve validar um nome: %s", (name: string) => {
+    const isValid = isValidName(name);
+    expect(isValid).toBe(true);
+});
+
+test.each([
+    "John",
+    null,
+    undefined,
+    "",
+    "   "
+])("Não deve validar um nome: %s", (name: any) => {
+    const isValid = isValidName(name);
+    expect(isValid).toBe(false);
+});
