8595a0e8bcee4ef30b84b3d0b78d704f3bb66d62
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-29 16:26:06 -0300

    ⚡  chore: include full order instead of just marketId

--- backend/src/domain/events/order-placed-event.ts
+++ backend/src/domain/events/order-placed-event.ts
@@ -1,11 +1,12 @@
+import { Order } from "../order";
 import { Event } from "./event";
 
 export class OrderPlacedEvent extends Event {
-  constructor(private readonly marketId: string) {
+  constructor(private readonly order: Order) {
     super("order-placed");
   }
 
   getPayload<T>(): T {
-    return this.marketId as T;
+    return this.order as T;
   }
 }
