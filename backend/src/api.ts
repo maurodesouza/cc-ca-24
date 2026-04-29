8ba8fed06168afdcb3654878cddca4c935f5919f
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-29 16:26:49 -0300

    🐛 fix: include missing registries

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -17,6 +17,9 @@ import { Mediator } from "./infra/mediator/mediator";
 import { OrderController } from "./infra/controllers/order-controller";
 import { UpdateOrder } from "./application/use-cases/update-order";
 import { Book } from "./domain/book";
+import { GetOrder } from "./application/use-cases/get-order";
+import { OrderRepositoryORM } from "./infra/repository/order-repository";
+import { PlaceOrder } from "./application/use-cases/place-order";
 
 const PORT = 4156;
 
@@ -36,12 +39,15 @@ function main() {
   Registry.getInstance().register("databaseConnection", new PGPromiseAdapter());
   Registry.getInstance().register("accountRepository", new AccountRepositoryORM());
   Registry.getInstance().register("walletRepository", new WalletRepositoryORM());
+  Registry.getInstance().register("orderRepository", new OrderRepositoryORM());
 
   Registry.getInstance().register("signUp", new SignUp());
   Registry.getInstance().register("deposit", new Deposit());
   Registry.getInstance().register("withdraw", new Withdraw());
+  Registry.getInstance().register("getOrder", new GetOrder());
   Registry.getInstance().register("getAccount", new GetAccount());
   Registry.getInstance().register("updateOrder", new UpdateOrder());
+  Registry.getInstance().register("placeOrder", new PlaceOrder());
 
   new AccountController();
   new BalanceController();
