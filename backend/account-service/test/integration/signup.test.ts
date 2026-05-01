5d7ebf91d7e6e1c887f0200b90caa2d9c57f3986
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-05-01 17:53:10 -0300

    🧪 test: remove subject assertion from mailer mock in signup test

--- backend/account-service/test/integration/signup.test.ts
+++ backend/account-service/test/integration/signup.test.ts
@@ -65,7 +65,6 @@ describe("SignUp", () => {
 
     expect(mailerMock.send).toHaveBeenCalledWith(expect.objectContaining({
       to: signupOutput.email,
-      subject: "Welcome to our platform!",
     }));
   });
 
