baa74e25a75c402d43af1ddf31301ad750010cdc
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-05-01 14:36:45 -0300

    ⚡ chore: split database schema into separate account and order SQL files

--- database/order.sql
+++ database/order.sql
@@ -4,10 +4,6 @@ create schema ccca;
 
 create table ccca.account (
     account_id uuid primary key,
-    name text,
-    email text,
-    document text,
-    password text
 );
 
 create table ccca.balance (
